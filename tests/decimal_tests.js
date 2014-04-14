d128e221e3f0e03feff4463bf4563706325bd053
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2014-04-14 15:53:53 +0900

    Fixed the rounding problem which occurred when the object is zero.

--- tests/decimal_tests.js
+++ tests/decimal_tests.js
@@ -1,6 +1,7 @@
 var Decimal = Decimal || require('../lib/decimal.js');
 var assert = assert || require('assert');
 
+assert.equal((Decimal.round(new Decimal(0), 8, Decimal.MidpointRounding.ToEven)).toString(), "0", "(Decimal.round(new Decimal(0), 8, Decimal.MidpointRounding.ToEven)).toString() == \"0\" FAILED")
 assert.equal((new Decimal("8")).sub("0").toString(), "8", "(new Decimal(\"8\").sub(\"0\").toString() == \"8\" FAILED")
 assert.equal((new Decimal("-0")).toString(), "0", "(new Decimal(\"-0\")).toString() == \"0\" FAILED")
 assert.equal((new Decimal("0.3")).sub((new Decimal("0.1")).mul(3)).toString(), "0", "(new Decimal(\"0.3\")).sub((new Decimal(\"0.1\")).mul(3)).toString() == \"0\" FAILED")
